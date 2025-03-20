12cf6af42f84a1fab6d16d11a55a0f531fe3e00c
Albert Tran <albertq.tran1@gmail.com>
2025-03-20 09:39:04 -0400

    changed phone image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import react, {useState, useEffect} from 'react';
-import logo from './logo.png';
-import mainImage1 from './Onboarding1.png'
-import mainImage2 from './Onboarding2.png'
-import mainImage3 from './Onboarding3.png'
-import downloadAppStore from "./DownloadAppStore.webp"
+import logo from './images/logo.png';
+import phoneImage from './images/phone.png'
+import profile1 from './images/profile1.jpeg'
+import profile2 from './images/profile2.jpeg'
+import profile3  from './images/profile3.jpeg'
+import downloadAppStore from "./images/DownloadAppStore.webp"
+import downloadGPStore from "./images/DownloadGPStore.png"
+
 import './App.css';
 
 function App() {
@@ -38,6 +41,40 @@ function App() {
     )
   }
 
+  const PhoneImage = () => {
+    return (
+      <div className="ImageHolder">
+        <div className="InsideImageHolder">
+          <img src={phoneImage}  className="ContentImage"/>
+          <img src={profile1} className="FloatingProfilePictureImage" 
+                style={{
+                  right: '57%',
+                  top: '42%',
+                  height: '11%',
+                  animationDelay: '1s',
+                  }}
+          />
+          <img src={profile2} className="FloatingProfilePictureImage"
+              style={{
+                right: '18%',
+                top: '58%',
+                height: '9%',
+                animationDelay: '0.6s',
+                }}
+          />
+          <img src={profile3} className="FloatingProfilePictureImage"
+              style={{
+                right: '18%',
+                top: '22%',
+                height: '15%',
+                animationDelay: '0.3s',
+                }}
+          />
+        </div>
+      </div>
+    )
+  }
+
   const ContentBox = () => {
     if(isMobile) return (
       <div className="ContentBoxContainer">
@@ -46,14 +83,13 @@ function App() {
             <div className="ContentTitle">
               See what's happening near you.
             </div>
-            <div className="ImageHolder">
-              <img src={mainImage3}  className="ContentImage"/>
-            </div>
+            <PhoneImage/>
             <div className="ContentDescription">
-              Check where your friends are. <br/>
+              Check what your friends are up to. <br/>
               Join the excitement. <br/>
             </div>
-            <GoToIOSButton/>
+              <img src={downloadAppStore} className="DownloadFromButton"/>
+              <img src={downloadGPStore} className="DownloadFromButton"/>
           </div>
         </div>
       </div>
@@ -66,15 +102,15 @@ function App() {
               See what's happening near you.
             </div>
             <div className="ContentDescription">
-              Check where your friends are. <br/>
+              Check what your friends are up to. <br/>
               Join the excitement. <br/>
             </div>
-            <GoToIOSButton/>
+            <div className="DownloadFromHolder">
+              <img src={downloadAppStore} className="DownloadFromButton"/>
+              <img src={downloadGPStore} className="DownloadFromButton"/>
+            </div>
           </div>
-          <div className="ImageHolder">
-            <img src={mainImage3}  className="ContentImage"/>
-          </div>
-
+          <PhoneImage/>
         </div>
       </div>
     )
@@ -84,23 +120,27 @@ function App() {
   return (
     <div className="App">
       <div className="BackgroundBox">
-        <div className="HeaderBoxContainer">
-          <div className="HeaderBox">
-            <img src={logo} className="Logo"/>
-            <GoToIOSButton/>
+        <div className="ScreenContainer">
+          <div className="HeaderBoxContainer">
+            <div className="HeaderBox">
+              <img src={logo} className="Logo"/>
+              <div className="HeaderRightSection">
+                <GoToIOSButton/>
+              </div>
+            </div>
           </div>
+          <ContentBox/>
+          <footer className="Footer">
+            <div className="FooterContent">
+              <p>&copy; 2025 OnMyWay. All rights reserved.</p>
+              {/* <ul className="FooterLinks">
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/services">Services</a></li>
+                <li><a href="/contact">Contact</a></li>
+              </ul> */}
+            </div>
+          </footer>
         </div>
-        <ContentBox/>
-        <footer className="Footer">
-          <div className="FooterContent">
-            <p>&copy; 2025 OnMyWay. All rights reserved.</p>
-            {/* <ul className="FooterLinks">
-              <li><a href="/about">About Us</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul> */}
-          </div>
-        </footer>
       </div>
     </div>
   );
