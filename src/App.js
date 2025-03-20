9caf6957a32977c1efd700f4b0bd8e87d9f9305a
Albert Tran <albertq.tran1@gmail.com>
2025-03-19 23:48:48 -0400

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="BackgroundBox">
+        <div className="HeaderBoxContainer">
+          <div className="HeaderBox">
+            <img src={logo} className="Logo"/>
+            <div className="GetAppButton">
+              Get the App
+            </div>
+          </div>
+        </div>
+        <div className="ContentBoxContainer">
+          <div className="ContentBox">
+            <div className="ContentColumn">
+              <div className="ContentTitle">
+                See what's happening near you.
+              </div>
+              <div className="ContentSubTitle">
+              </div>
+              <div className="ContentDescription">
+                Check what your friends are up to. See where the fun is. 
+              </div>
+              <div className="GetAppButton">
+                Get the App
+              </div>
+            </div>
+            <div className="ContentImage">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +47,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
